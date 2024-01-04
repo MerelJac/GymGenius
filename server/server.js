f1e83f07c2b089d6f621d2a737c3472cfcb1debf
Merel Jacobs <merel.burleigh@gmail.com>
2024-01-04 12:49:00 -0800

    still not working

--- server/server.js
+++ server/server.js
@@ -69,61 +69,80 @@ app.get("/auth/url", (_, res) => {
   });
 });
 
-app.get('/auth/token', async (req, res) => {
-    const { code } = req.query;
-    if (!code) return res. status(400).json({ message: 'Authorization code must be provided' });
-    try {
-      // Get all parameters needed to hit authorization server
-      const tokenParam = getTokenParams(code);
-      // Exchange authorization code for access token (id token is returned here too)
-      const { data: { id_token} } = await axios.post(`${config.tokenUrl}?${tokenParam}`);
-      if (!id_token) return res.status(400).json({ message: 'Auth error' });
-      // Get user info from id token
-      const { email, name, picture } = jwt.decode(id_token);
-      const user = { name, email, picture };
-      // Sign a new token
-      const token = jwt.sign({ user }, config.tokenSecret, { expiresIn: config.tokenExpiration });
-      // Set cookies for user
-      res.cookie('token', token, { maxAge: config.tokenExpiration, httpOnly: true,  })
-      // You can choose to store user in a DB instead
-      res.json({
-        user,
-      })
-    } catch (err) {
-      console.error('Error: ', err);
-      res.status(500).json({ message: err.message || 'Server error' });
-    }
+app.get("/auth/token", async (req, res) => {
+  const { code } = req.query;
+  if (!code)
+    return res
+      .status(400)
+      .json({ message: "Authorization code must be provided" });
+  try {
+    // Get all parameters needed to hit authorization server
+    const tokenParam = getTokenParams(code);
+    // Exchange authorization code for access token (id token is returned here too)
+    const {
+      data: { id_token },
+    } = await axios.post(`${config.tokenUrl}?${tokenParam}`);
+    if (!id_token) return res.status(400).json({ message: "Auth error" });
+    // Get user info from id token
+    const { email, name, picture } = jwt.decode(id_token);
+    const user = { name, email, picture };
+    // Sign a new token
+    const token = jwt.sign({ user }, config.tokenSecret, {
+      expiresIn: config.tokenExpiration,
+    });
+    // Set cookies for user
+    res.cookie("token", token, {
+      maxAge: config.tokenExpiration,
+      httpOnly: true,
+    });
+    // You can choose to store user in a DB instead
+    res.json({
+      user,
+    });
+  } catch (err) {
+    console.error("Error: ", err);
+    res.status(500).json({ message: err.message || "Server error" });
+  }
 });
-    
-app.get('/auth/logged_in', (req, res) => {
-    try {
-      // Get token from cookie
-      const token = req.cookies.token;
-      if (!token) return res.json({ loggedIn: false });
-      const { user } = jwt.verify(token, config.tokenSecret);
-      const newToken = jwt.sign({ user }, config.tokenSecret, { expiresIn: config.tokenExpiration });
-      // Reset token in cookie
-      res.cookie('token', newToken, { maxAge: config.tokenExpiration, httpOnly: true,  })
-      res.json({ loggedIn: true, user });
-    } catch (err) {
-      res.json({ loggedIn: false });
-    }
-  });
 
-  app.post("/auth/logout", (_, res) => {
-    // clear cookie
-    res.clearCookie('token').json({ message: 'Logged out' });
-  });
+app.get("/auth/logged_in", (req, res) => {
+  try {
+    // Get token from cookie
+    console.log(req);
+    const token = req.cookies.token;
+    if (!token) return res.json({ loggedIn: false });
+    const { user } = jwt.verify(token, config.tokenSecret);
+    const newToken = jwt.sign({ user }, config.tokenSecret, {
+      expiresIn: config.tokenExpiration,
+    });
+    // Reset token in cookie
+    res.cookie("token", newToken, {
+      maxAge: config.tokenExpiration,
+      httpOnly: true,
+    });
+    // Set CORS headers
+    res.header("Access-Control-Allow-Credentials", "true");
 
-  // testing end route - change alter 
-  app.get('/user/posts', auth, async (_, res) => {
-    try {
-      const { data } = await axios.get(config.postUrl);
-      res.json({ posts: data?.slice(0, 5) });
-    } catch (err) {
-      console.error('Error: ', err);
-    }
-  });
+    res.json({ loggedIn: true, user });
+  } catch (err) {
+    res.json({ loggedIn: false });
+  }
+});
+
+app.post("/auth/logout", (_, res) => {
+  // clear cookie
+  res.clearCookie("token").json({ message: "Logged out" });
+});
+
+// testing end route - change alter
+app.get("/user/posts", auth, async (_, res) => {
+  try {
+    const { data } = await axios.get(config.postUrl);
+    res.json({ posts: data?.slice(0, 5) });
+  } catch (err) {
+    console.error("Error: ", err);
+  }
+});
 
 //middleware
 app.use(
