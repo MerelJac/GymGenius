3055e991a7351a8666be7e0cbc5299e11c9d0312
Merel Jacobs <merel.burleigh@gmail.com>
2026-02-06 13:07:45 -0800

    proper date

--- src/app/components/clients/ClientProfile.tsx
+++ src/app/components/clients/ClientProfile.tsx
@@ -16,6 +16,7 @@ import { useRouter } from "next/navigation";
 import { AlertCircle, Trash } from "lucide-react";
 import WorkoutCalendarWeek from "../CalendarScheduledWorkout";
 import { SyncProgramButton } from "../programs/SyncProgramButton";
+import { formatDateFromInputReturnString } from "@/app/utils/format/formatDateFromInput";
 
 export default function ClientProfile({
   client,
@@ -308,7 +309,7 @@ export default function ClientProfile({
                         <div>
                           <span className="font-medium">{w.workout.name}</span>
                           <span className="text-gray-500 ml-2">
-                            • {new Date(w.scheduledDate).toLocaleDateString()}
+                            • {formatDateFromInputReturnString(w.scheduledDate)}
                           </span>
                         </div>
                         <span
