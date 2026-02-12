90c53de8276a80920489bc7036fdb4d13148fc32
Merel Jacobs <merel.burleigh@gmail.com>
2026-02-12 14:26:56 -0800

    add workout email

--- src/app/actions/workout.ts
+++ src/app/actions/workout.ts
@@ -1,6 +1,7 @@
 // src/app/actions/workouts.ts
 "use server";
 
+import { sendAdditionalWorkoutEmailToTrainer } from "@/lib/email-templates/additionalWorkoutEmailToTrainer";
 import { prisma } from "@/lib/prisma";
 
 export async function addAdditionalWorkout(
@@ -11,7 +12,7 @@ export async function addAdditionalWorkout(
     distance?: number;
     notes?: string;
     performedAt: Date;
-  }
+  },
 ) {
   await prisma.additionalWorkout.create({
     data: {
@@ -19,4 +20,43 @@ export async function addAdditionalWorkout(
       ...data,
     },
   });
+
+  const workoutType = await prisma.additionalWorkoutType.findUnique({
+    where: { id: data.typeId },
+  });
+  const client = await prisma.user.findUnique({
+    where: { id: clientId },
+    include: {
+      profile: true,
+    },
+  });
+
+  const trainer = client?.trainerId
+    ? await prisma.user.findUnique({
+        where: { id: client.trainerId },
+        select: {
+          email: true,
+        },
+      })
+    : null;
+
+  if (!trainer?.email || !client?.profile?.firstName || !workoutType?.name) {
+    return;
+  }
+
+  try {
+    await sendAdditionalWorkoutEmailToTrainer(
+      trainer.email,
+      client.profile.firstName,
+      workoutType.name,
+      data.performedAt,
+    );
+
+    console.log("Sent additional workout email to trainer", client.email);
+  } catch (error) {
+    console.error(
+      "❌ Failed to send additional workout email to trainer",
+      error,
+    );
+  }
 }
