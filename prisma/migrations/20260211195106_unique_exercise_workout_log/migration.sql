43aa7102efcb3cbd5b42869b8d77276222562d78
Merel Jacobs <merel.burleigh@gmail.com>
2026-02-12 07:01:14 -0800

    removes migration file

--- prisma/migrations/20260211195106_unique_exercise_workout_log/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[workoutLogId,exerciseId]` on the table `ExerciseLog` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "ExerciseLog_workoutLogId_exerciseId_key" ON "ExerciseLog"("workoutLogId", "exerciseId");
