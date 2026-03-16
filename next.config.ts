ab6178e6f2d3a64074a12e6a59c21dae614fc3ef
Merel Jacobs <merel.burleigh@gmail.com>
2026-03-16 16:17:06 -0700

    loggign to cloudwatch

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: {
+    fetches: { fullUrl: true },
+  },
+  // surfaces server errors in prod logs
+  serverRuntimeConfig: {
+    // your config
+  },
 };
 
 export default nextConfig;
